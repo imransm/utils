54fd298d09ea02d67f80caa3cb0e469cc9d2c253
Imran <imran.sk@gmail.com>
2014-09-13 16:55:39 -0700

    quick sort

---
+++ src/main/java/QuickSort.java
@@ -0,0 +1,48 @@
+/**
+ * Created by Imran on 9/13/2014.
+ *
+ * CLRS algorithm, selecting last element as pivot.
+ *
+ */
+public class QuickSort {
+
+    public void sort(int[] elements) {
+        quickSort(elements, 0, elements.length - 1);
+    }
+
+    private void quickSort(int[] elements, int start, int end) {
+        if(start < end) {
+            int q = partition(elements, start, end);
+            quickSort(elements, start, q - 1);
+            quickSort(elements, q + 1, end);
+        }
+    }
+
+    private int partition(int[] elements, int start, int end) {
+        int pivot = elements[end];
+        int i = start -1;
+        for(int j = start; j<end; j++) {
+            if(elements[j] <= pivot) {
+                i++;
+                swap(elements, i, j);
+            }
+        }
+        swap(elements, i+1, end);
+        return i+1;
+    }
+
+    private void swap(int[] elements, int i, int j) {
+        int temp = elements[j];
+        elements[j]=  elements[i];
+        elements[i] = temp;
+    }
+
+    public static void main(String[] args) {
+      QuickSort quickSort = new QuickSort();
+        int[] elements = {5, 6, 7, 9, 10, 12, 1, 4, 2, 3};
+        quickSort.sort(elements);
+        for(int i : elements) {
+            System.out.println(i);
+        }
+    }
+}
