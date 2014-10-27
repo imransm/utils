2840412e23fc096825211eb2833c3192bd78e6bf
Imran <imran.sk@gmail.com>
2014-10-27 00:22:04 -0700

    Knuth's shuffle.

--- src/main/java/Shuffle.java
+++ src/main/java/Shuffle.java
@@ -7,11 +7,22 @@ public class Shuffle {
 
     public int[] shuffle(int[] input) {
         for(int i=input.length -1; i> 0; i--) {
-            int j = Math.round((float)Math.floor(Math.random()*i));
-            int temp = input[j];
-            input[j] = input[i];
-            input[i] = temp;
+            int j = numberInRange(i);
+            if(i != j) {
+                int temp = input[j];
+                input[j] = input[i];
+                input[i] = temp;
+            }
         }
         return input;
+    }
+
+    /**
+     * Generates random number from 0 to limit, limit exclusive.
+     * @param limit
+     * @return
+     */
+    int numberInRange(int limit) {
+        return Math.round((float)Math.floor(Math.random()*(limit)));
     }
 }
--- src/test/java/ShuffleTest.java
+++ src/test/java/ShuffleTest.java
@@ -8,11 +8,21 @@ public class ShuffleTest {
     public void shouldShuffleGivenInput() {
         int[] array = new int[]{1, 2, 3, 4, 5, 6, 7};
         Shuffle shuffle = new Shuffle();
-        for(int i=0; i<array.length; i++) {
+        for(int i=0; i<15; i++) {
             int[] result1 = shuffle.shuffle(array);
             printArray(result1);
         }
 
+    }
+
+    @Test
+    public void shouldGenerateNumberInRange() {
+        Shuffle shuffle = new Shuffle();
+        int limit = 10;
+        for(int i=0; i<10; i++) {
+            int generatedNumber = shuffle.numberInRange(limit);
+            assertTrue(generatedNumber < limit);
+        }
     }
 
     private void printArray(int[] result) {
